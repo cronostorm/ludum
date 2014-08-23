3986eeb41e238612724eccbd538ae03f3878cfd9
cronostorm <cronostorm@gmail.com>
2014-08-23 14:04:21 -0700

    Basic character movement and level
    Level is just a long rectangle that blocks player from falling down
    Player is a rectangle that can move left and right

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+  public float speed = 1.0f;
+  
+  private bool facingRight = true;
+
+	void Start () {
+	}
+	
+  void FixedUpdate() {
+    float move = Input.GetAxis("Horizontal");
+    rigidbody2D.velocity = new Vector2(move * speed, rigidbody2D.velocity.y);
+
+    if ((move > 0 && !facingRight) || (move < 0 && facingRight)) {
+      Flip();
+    }
+  }
+
+  void Flip() {
+    facingRight = !facingRight;
+    Vector3 scale = transform.localScale;
+    scale.x *= -1;
+    transform.localScale = scale;
+  }
+}
