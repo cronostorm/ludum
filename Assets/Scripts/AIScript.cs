876c9895793d15b8ebcad574899e97ccefbb2df6
cronostorm <cronostorm@gmail.com>
2014-08-24 18:11:07 -0700

    Enemy now patrols and chases player
    Enemy patrols between two points, chases player at a faster
    speed if it sees the player in it's vision cone

---
+++ Assets/Scripts/AIScript.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIScript : MonoBehaviour {
+  public float normalSpeed; 
+  public float chaseSpeed; 
+  public float sightDistance;
+  private bool facingRight;
+  public Rigidbody2D body;
+  public LampScript lamp;
+
+  public Transform start;
+  public Transform end;
+
+  private float lastFlip;
+  private int horizontal = 0;
+  
+
+	// Use this for initialization
+	void Start () {
+    facingRight = transform.localScale.x > 0;
+	}
+	
+  void FixedUpdate() {
+    if (Mathf.Abs(transform.position.x - start.position.x) < 0.1f) {
+      Flip();
+    }
+    else if (Mathf.Abs(transform.position.x - end.position.x) < 0.1f) {
+      Flip();
+    }
+
+    if (lamp.spottedPlayer) ChasePlayer();
+    else Patrol();
+  }
+
+  void Patrol() {
+    horizontal = -1;
+    if (facingRight) {
+      horizontal = 1;
+    }
+
+    body.AddForce(new Vector2(horizontal * 75, 0));
+    Vector2 vel = body.velocity;
+    vel.x = Mathf.Clamp(vel.x, -normalSpeed, normalSpeed);
+    body.velocity = vel;
+  }
+
+  void ChasePlayer() {
+    horizontal = -1;
+    if (facingRight) {
+      horizontal = 1;
+    }
+
+    body.AddForce(new Vector2(horizontal * 1000, 0));
+    Vector2 vel = body.velocity;
+    vel.x = Mathf.Clamp(vel.x, -chaseSpeed, chaseSpeed);
+    body.velocity = vel;
+  }
+
+  void Flip() {
+    if (Time.time - lastFlip < 0.2f) {
+      return;
+    }
+    lastFlip = Time.time;
+    facingRight = !facingRight;
+    Vector3 scale = transform.localScale;
+    scale.x *= -1;
+    transform.localScale = scale;
+    lamp.offsetAngle = -90 * transform.localScale.x;
+    lamp.transform.localScale = new Vector3(1/transform.localScale.x, 1/transform.localScale.y, 1/transform.localScale.z);
+    
+  }
+}
--- Assets/Scripts/LampScript.cs
+++ Assets/Scripts/LampScript.cs
@@ -6,10 +6,12 @@ public class LampScript : MonoBehaviour {
 	//static lamp with specified degree for cone of light
 
 	public float lightAngle;
+  public float offsetAngle;
 	public float distance;
   public int numRays;
 	public Material lightMaterial;
-  private float lastTime = 0;
+
+  public bool spottedPlayer { get; protected set; }
 	protected int[] triangles;
 	protected Vector2[] points;
 	protected Vector3[] verts;
@@ -22,53 +24,52 @@ public class LampScript : MonoBehaviour {
 		gameObject.AddComponent ("MeshFilter");
 		gameObject.AddComponent ("MeshRenderer");
 		GetComponent<MeshRenderer> ().material = lightMaterial;
-    GenerateConeFirst();
-	  InvokeRepeating("SetCollider", 0, 0.5f);
+		Mesh mesh = new Mesh();
+    GetComponent<MeshFilter>().sharedMesh = mesh;
+    SetCollider();
+	  GenerateCone();
+		mesh.uv = points;
+    mesh.triangles = triangles;
 	}
 
 	void SetCollider () {
 		float angle = lightAngle * Mathf.PI / 180;
     float delta = angle/(numRays - 1);
+    angle = offsetAngle * 2 * Mathf.PI / 180 + angle;
     
+    spottedPlayer = false;
+
     Vector2 origin = transform.position;
     RaycastHit2D[] hits = new RaycastHit2D[1];
     float xangle = Mathf.Sin(-angle/2);
     float yangle = -Mathf.Cos(-angle/2);
-    points[0] = new Vector2(0,0);
-    verts[0] = (Vector3) points[0];
+    verts[0] = new Vector3(0,0,0 );
     for (int i = 1; i < numRays + 1; i++) {
       xangle = Mathf.Sin(-angle/2 + delta * (i - 1));
       yangle = -Mathf.Cos(-angle/2 + delta * (i - 1));
       if (Physics2D.RaycastNonAlloc(origin, new Vector2(xangle, yangle).normalized, hits, distance) > 0) {
-        points[i] = new Vector2(xangle, yangle).normalized * hits[0].fraction * distance;
+        verts[i] = new Vector3(xangle, yangle, 0).normalized * hits[0].fraction * distance;
+        spottedPlayer = spottedPlayer || hits[0].collider.tag == "Player";
       }
       else {
-        points[i] = new Vector2(xangle, yangle) * distance;
+        verts[i] = new Vector3(xangle, yangle, 0) * distance;
       }
-      verts[i] = (Vector3) points[i];
+
       if (i > 1) {
         triangles[(i - 2) * 3] = 0;
         triangles[(i - 2) * 3 + 1] = i - 1;
         triangles[(i - 2) * 3 + 2] = i;
       }
     }
-
-		gameObject.GetComponent<PolygonCollider2D> ().SetPath (0, points);
-	  GenerateCone();
 	}
 	
-	void GenerateConeFirst () {
-		Mesh mesh = new Mesh();
-    GetComponent<MeshFilter>().sharedMesh = mesh;
-	}
-
 	void GenerateCone () {
 		Mesh mesh = GetComponent<MeshFilter>().sharedMesh;
 		mesh.vertices = verts;
-		mesh.uv = points;
-    mesh.triangles = triangles;
 	}
 
-  void Update() { 
+  void FixedUpdate() { 
+    SetCollider();
+	  GenerateCone();
   }
 }
